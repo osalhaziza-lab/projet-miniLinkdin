130284d058d5a228209fc6d593111e080a0eee99
SmaliAYA <smali.aya@ensam-casa.ma>
2026-04-19 13:00:18 +0100

    fix: ajout route profil/competences

--- routes/api.php
+++ routes/api.php
@@ -23,6 +23,7 @@ Route::middleware('auth:api')->group(function () {
     Route::post('/profil', [ProfilController::class, 'store']);
     Route::get('/profil',  [ProfilController::class, 'show']);
     Route::put('/profil',  [ProfilController::class, 'update']);
+    Route::post('/profil/competences', [ProfilController::class, 'addCompetence']);
     Route::delete('/profil/competences/{competence_id}', [ProfilController::class, 'removeCompetence']);
 
     // Offres
